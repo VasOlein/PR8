96ceaaf313c09cbf658fc6ad6ed67b5be2d62c4a
kab231_09 <student09@kru.ru>
2025-03-05 10:26:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// Объявление и инициализация массива
+			int[] arr = new int[] { 1, 8, 6, 7, 5, 9 };
+
+			// Сортировать массив по возрастанию
+			Array.Sort(arr);
+			Console.WriteLine("По возрастанию: ");
+			foreach (int value in arr)
+			{
+				Console.Write(value + " ");
+			}
+		}
+    }
+}
